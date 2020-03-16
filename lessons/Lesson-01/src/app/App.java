ba567d4ef43d21e957770944dd39020d28b97561
Lawrence (Drew) Whisenant <7390131+drewwhis@users.noreply.github.com>
2020-03-16 17:18:20 -0500

    add notes on package, class, and main

--- lessons/Lesson-01/src/app/App.java
+++ lessons/Lesson-01/src/app/App.java
@@ -1,7 +1,45 @@
+/**
+ * In Java, all files should belong to a package.
+ * The name of the package is made of directories.
+ * 
+ * Package names start with the first directory inside 
+ * the src directory and include any sub-folders,
+ * separated by period.
+ * 
+ * This file lives in the src/app directory, so its package
+ * is app. Every file should have its package listed at the
+ * top of the file.
+ */
 package app;
 
+/**
+ * In Java, everything is treated as an object.
+ * 
+ * Every .java file defines one of these objects as a blueprint.
+ * In this case (and in most) this blueprint is called a class.
+ * 
+ * The name of the class must be the same as the name of the file.
+ * 
+ * In this case, the class's name is App, so the file 
+ * must be App.java.
+ */
 public class App {
+    /**
+     * In order for a Java program to run, 
+     * there must be an entry-point.
+     * 
+     * This entry-point is the main method. A method is 
+     * something that is part of a class that can 
+     * perform some action. The main method is a special
+     * one of these methods.
+     * 
+     * When a Java program runs, it looks for the main method
+     * that looks like this one to call.
+     * 
+     * For now, all of the code you write will go
+     * in this special, main method.
+     */
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello world!");
+        
     }
 }
