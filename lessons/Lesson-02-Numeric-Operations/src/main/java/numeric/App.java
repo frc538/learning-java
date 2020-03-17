f48c546347a45f9bda461542fcd70a456a408473
Lawrence (Drew) Whisenant <7390131+drewwhis@users.noreply.github.com>
2020-03-16 22:42:41 -0500

    add general comments to class

--- lessons/Lesson-02-Numeric-Operations/src/main/java/numeric/App.java
+++ lessons/Lesson-02-Numeric-Operations/src/main/java/numeric/App.java
@@ -1,5 +1,18 @@
+/**
+ * Same as before, this file's package
+ * is numeric because the directory inside
+ * src/main/java is called numeric.
+ */
 package numeric;
 
+/**
+ * And the class name is the same as the file.
+ */
 public class App {
-
+    /**
+     * We also have the main method that will allow the 
+     * program to run.
+     */
+    public static void main(String[] args) throws Exception {
+    }
 }
