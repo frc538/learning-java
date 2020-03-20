63da49dc5f1efa80a1fa3ca6765cabfbd4bb9820
Lawrence (Drew) Whisenant <7390131+drewwhis@users.noreply.github.com>
2020-03-19 23:21:48 -0500

    int comparison > < <= >=

--- lessons/Lesson-04-Boolean-Logic/src/main/java/booleanlogic/App.java
+++ lessons/Lesson-04-Boolean-Logic/src/main/java/booleanlogic/App.java
@@ -41,7 +41,115 @@ public class App {
          * value of a constant.
          */
 
-        // Boolean values
+        /**
+         * There is one primitive type you haven't seen yet - the boolean.
+         * 
+         * A boolean represents one of two values - true or false. You might think about
+         * these values as 1 and 0 or on and off. While this is a good way to think
+         * about it (and in some languages, it is the same!), Java sticks strictly to
+         * true and false.
+         */
+        boolean trueBoolean = true;
+        boolean falseBoolean = false;
+
+        System.out.println(trueBoolean);
+        System.out.println(falseBoolean);
+
+        /**
+         * Booleans can have no other values, so they're a pretty simple type of data to
+         * work with. Anything that you can phrase as a question with a yes or no answer
+         * can give you a boolean!
+         * 
+         * Is it raining today?
+         * 
+         * Is 4 bigger than 5?
+         * 
+         * Is 5 smaller than 5?
+         * 
+         * All of these will give true values if the answer is yes or false if the
+         * answer is no.
+         */
+
+        /**
+         * We can start by comparing integers.
+         * 
+         * We have the symbols > (greater than), < (less than), >= (greater than or
+         * equal to), and <= (less than or equal to).
+         * 
+         * For example, 3 > 4 is false and 4 > 3 is true.
+         */
+        boolean comparisonResult = 3 > 4;
+        System.out.println(comparisonResult);
+
+        comparisonResult = 3 < 4;
+        System.out.println(comparisonResult);
+
+        comparisonResult = 3 >= 4;
+        System.out.println(comparisonResult);
+
+        comparisonResult = 3 <= 4;
+        System.out.println(comparisonResult);
+
+        comparisonResult = 3 > 3;
+        System.out.println(comparisonResult);
+
+        comparisonResult = 3 < 3;
+        System.out.println(comparisonResult);
+
+        comparisonResult = 3 >= 3;
+        System.out.println(comparisonResult);
+
+        comparisonResult = 3 <= 3;
+        System.out.println(comparisonResult);
+
+        /**
+         * This, of course, also works if you use variables.
+         */
+        int firstNumber = 3;
+        int secondNumber = 4;
+        comparisonResult = firstNumber > secondNumber;
+        System.out.println(comparisonResult);
+
+        comparisonResult = firstNumber < secondNumber;
+        System.out.println(comparisonResult);
+
+        comparisonResult = firstNumber >= secondNumber;
+        System.out.println(comparisonResult);
+
+        comparisonResult = firstNumber <= secondNumber;
+        System.out.println(comparisonResult);
+
+        secondNumber = 3;
+        comparisonResult = firstNumber > secondNumber;
+        System.out.println(comparisonResult);
+
+        comparisonResult = firstNumber < secondNumber;
+        System.out.println(comparisonResult);
+
+        comparisonResult = firstNumber >= secondNumber;
+        System.out.println(comparisonResult);
+
+        comparisonResult = firstNumber <= secondNumber;
+        System.out.println(comparisonResult);
+
+        /**
+         * And a comparison will produce ta rue/false result even if you don't store it
+         * in a variables.
+         */
+        System.out.println(3 > 3);
+        System.out.println(firstNumber <= secondNumber);
+
+        /**
+         * You can also compare doubles in this way.
+         */
+        System.out.println(1.0 > 2.0);
+
+        /**
+         * It is sometimes possible to compare values for different types (ints and
+         * doubles), but you should generally avoid this unless you absolutely have to.
+         */
+
+         
 
         // Integer comparison
 
